589374be042aae7c16b55d65f6c7a564c7f4ad95
minhduc2611 <minhduc261298@gmail.com>
2019-05-13 16:29:24 +0700

    sum of the elements of an given array of integers.

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,42 +2,19 @@
 
 class demo
 {
-    //viet mot ham de trao doi hai gia tri bang POINTER
-    /*
-    * POINTER: 
-    *
-    * A pointer is nothing but a variable that holds the memory address of another type
-    * Declaring a Pointer type : type *variable_name; --> e.g : int *num1
-    * 
-    * The &num1 gives the memory address of the variable num1, which we can assign to a pointer variable 
-    * 
-    * Remember that in C# any code involving pointers requires an unsafe context.
-    *
-    * apply :
-    * Binary search, heap ,tree,graph,linked list ...... 
-    *
-    *
-     */
-    
+    // sum of the elements of an given array of integers.
 
-     static unsafe void swap(int* num1,int* num2){
-         int temp;
-         temp = *num1;
-         *num1 = *num2;
-         *num2 = temp;
+    public static int sumA(int[] arr){
+        int len = arr.Length;
+        int sumArr = 0;
+        for(int i = 0 ; i <= len-1; i++){
+            sumArr += arr[i];
+        };
+        return sumArr;
     }
-    static unsafe void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("num 1");
-        int num1 = Convert.ToInt32( Console.ReadLine());
-        Console.WriteLine("num 2");
-        int num2 = Convert.ToInt32( Console.ReadLine());
-
-        swap(&num1, &num2);
-
-        Console.WriteLine($"\nSau khi trao doi:");
-        Console.WriteLine($"\nSo thu nhat:"+ num1);
-        Console.WriteLine($"\nSo thu hai:"+ num2);
+        int[] arr =  new int[]{1,2,3};
+        Console.WriteLine(sumA(arr));
     }
-
 }
