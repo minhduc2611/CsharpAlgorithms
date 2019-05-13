72a7f0c1719651afa520a6a48eba44181301a124
minhduc2611 <minhduc261298@gmail.com>
2019-05-13 15:43:21 +0700

    POINTER CONLUSION

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -3,6 +3,23 @@
 class demo
 {
     //viet mot ham de trao doi hai gia tri bang POINTER
+    /*
+    * POINTER: 
+    *
+    * A pointer is nothing but a variable that holds the memory address of another type
+    * Declaring a Pointer type : type *variable_name; --> e.g : int *num1
+    * 
+    * The &num1 gives the memory address of the variable num1, which we can assign to a pointer variable 
+    * 
+    * Remember that in C# any code involving pointers requires an unsafe context.
+    *
+    * apply :
+    * Binary search, heap ,tree,graph,linked list ...... 
+    *
+    *
+     */
+    
+
      static unsafe void swap(int* num1,int* num2){
          int temp;
          temp = *num1;
