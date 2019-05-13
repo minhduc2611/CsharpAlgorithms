a5a943a454c30edccce4b48ccb33f00ba3614c27
minhduc2611 <minhduc261298@gmail.com>
2019-05-13 16:47:34 +0700

    Checking if array is symmetric.

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,19 +2,26 @@
 
 class demo
 {
-    // sum of the elements of an given array of integers.
+    // Checking if array is symmetric
 
-    public static int sumA(int[] arr){
-        int len = arr.Length;
-        int sumArr = 0;
-        for(int i = 0 ; i <= len-1; i++){
-            sumArr += arr[i];
-        };
-        return sumArr;
+    static Boolean CheckIfSymetric(int[] arr){
+        for(int i = 0; i <arr.Length/2 ; i++ ){
+            if(arr[i] != arr[(arr.Length-1)-i]){
+                return false;
+            }
+        }
+    return true;
+        
     }
     static void Main(string[] args)
     {
-        int[] arr =  new int[]{1,2,3};
-        Console.WriteLine(sumA(arr));
+        int[] arrSym = new int[]{1,2,3,2,1};
+        int[] arrSym2 = new int[]{1,2,2,1};
+        int[] arr = new int[]{1,2,3,4,2,1};
+        //check 
+        Console.WriteLine(CheckIfSymetric(arrSym)); 
+        Console.WriteLine(CheckIfSymetric(arrSym2)); 
+        Console.WriteLine(CheckIfSymetric(arr)); 
+
     }
 }
