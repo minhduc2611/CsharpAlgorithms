97df41c557c80b44a75f2057e352502c930e1455
minhduc2611 <minhduc261298@gmail.com>
2019-05-13 19:41:08 +0700

    Find the two repeating elements in a given array

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,26 +2,24 @@
 
 class demo
 {
-    // Checking if array is symmetric
-
-    static Boolean CheckIfSymetric(int[] arr){
-        for(int i = 0; i <arr.Length/2 ; i++ ){
-            if(arr[i] != arr[(arr.Length-1)-i]){
-                return false;
+    // Find the two repeating elements in a given array
+    static void CheckIfHasRepeat(int[] arr){
+        int i,j;
+        Console.WriteLine("the repeated elements are : ");
+        for(i = 0 ; i < arr.Length ; i++){
+            for(j = i+1; j<arr.Length; j++){
+                if(arr[j] == arr[i]){
+                    Console.WriteLine(arr[i]+" ");      
+                }
             }
         }
-    return true;
-        
+
+
     }
     static void Main(string[] args)
     {
-        int[] arrSym = new int[]{1,2,3,2,1};
-        int[] arrSym2 = new int[]{1,2,2,1};
-        int[] arr = new int[]{1,2,3,4,2,1};
-        //check 
-        Console.WriteLine(CheckIfSymetric(arrSym)); 
-        Console.WriteLine(CheckIfSymetric(arrSym2)); 
-        Console.WriteLine(CheckIfSymetric(arr)); 
+        int[] Arr = new int[] {1,2,3,1,2};
 
+        CheckIfHasRepeat(Arr);
     }
 }
