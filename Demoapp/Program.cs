8b0c0b1cfefcd452bdc7935c3938cce4c38e22fe
minhduc2611 <minhduc261298@gmail.com>
2019-05-13 13:40:14 +0700

    POINTER VÀ DELEGATE C#

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,11 +2,27 @@
 
 class demo
 {
+    // POINTER VÀ DELEGATE C# 
+    static Func<int,int, int> FunctionPointers = null; // thuộc dạng Delegate :  lấy vào 2 int va trả về 1 int => tổng cộng co 3 int nhé 
     static unsafe void Main(string[] args)
     {
-        int x =100;
-        int* px = &x;
+        FunctionPointers = sub;
+        Console.WriteLine(FunctionPointers(1,2));  
+        FunctionPointers = min;
+        Console.WriteLine(FunctionPointers(1,2));  
+        FunctionPointers = mul;
+        Console.WriteLine(FunctionPointers(1,2));  
+    }
 
-        Console.WriteLine(*px);
+    static public int sub(int a, int b){
+        return a+b;
+    }
+
+    static public int min(int a, int b){
+        return a-b;
+    }
+
+    static public int mul(int a, int b){
+        return a*b;
     }
 }
