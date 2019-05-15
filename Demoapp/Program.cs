d83a503c1b181d72444bd27182b6eaa4bb1242b3
minhduc2611 <minhduc261298@gmail.com>
2019-05-15 18:29:46 +0700

    Binary tree 2

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,66 +2,94 @@
 
 class demo
 {
-    // Sort a array By Binary tree
+    // Binary tree 2
 
 
-   //--- ------- ----- ------- ---- -- node model--- ------- ---- --
-    public class Node{
-       public int Data;
-       public Node Left;
-       public Node Right;
-       public Node(int i){
-          this.Data = i;
-       }
-   //  --------- ------- ---- -- --- function insert --- ------- ---- --
-           public void Insert(int i)
+    //node model
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+        public void DisplayNode()
         {
-           // thuc hien insert khi co root
-           if( i <= Data ){
-              //left
-              if(this.Left == null){
-                 this.Left = new Node(i);
-              }else{
-                 this.Left.Insert(i);
-              }
-           }
-           else{
-            // right
-               if(this.Right == null){
-                  this.Right = new Node(i);
-               }else{
-                  this.Right.Insert(i);
-               }
-           }
+            Console.Write(Data + " ");
         }
-    //--- ------- ---- ----- ------- ---- -- Print In order--- ------- ---- --
-
-    public void PrintInOrder(){
-       //left first 
-      if(this.Left != null){
-         this.Left.PrintInOrder();
-      }
-       // main node 
-       Console.WriteLine(this.Data);
-       //then right
-       if(this.Right!= null){
-          this.Right.PrintInOrder();
-       }
     }
-   }
+    //binary tree model 
+    public class BinarySearchTree
+    {
+        public Node Root;
+        public BinarySearchTree()
+        {
+            this.Root = null;
+        }
+        public void Insert(int i)
+        {
+            Node NewNode = new Node() { Data = i };
+            if (this.Root == null)
+            {
+                this.Root = NewNode; //end
+            }
+            else
+            {
+                Node Current = Root; // pointer = current
+                Node Parent;
+                while (true)
+                {
+                    Parent = Current;
+                    if (i < Current.Data)
+                    {
+                        Current = Current.Left;
+                        if (Current == null)
+                        {
+                            Parent.Left = NewNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Current = Current.Right;
+                        if (Current == null)
+                        {
+                            Parent.Right = NewNode;
+                            break;
+                        }
+                    }
+                }
+            }
 
-   static void Main(string[] args) {
+        }
 
-      int[] arr = new int[]{4,2,3,5,7,2,1,7,5};
-      Node BinaryTree = new Node(arr[0]);
+        public void InOrder(Node TheRoot)
+        {
+            if (!(TheRoot == null))
+            {
+                InOrder(TheRoot.Left);
+                TheRoot.DisplayNode();
+                InOrder(TheRoot.Right);
+            }
+        }
+    }
 
-      for(int i = 1; i< arr.Length ; i++){
-         BinaryTree.Insert(arr[i]); 
-      }
-      BinaryTree.PrintInOrder();
-      
-   }
-   
+    static void Main(string[] args)
+    {
+
+        //int[] arr = new int[] { 4, 2, 3, 5, 7, 2, 1, 7, 5 };
+      BinarySearchTree numbers = new BinarySearchTree();
+      numbers.Insert(23);
+      numbers.Insert(45);
+      numbers.Insert(26);
+      numbers.Insert(37);
+      numbers.Insert(3);
+      numbers.Insert(99);
+      numbers.Insert(21);
+      numbers.Insert(22);
+
+      Console.WriteLine("InOrder Traversal:");
+      numbers.InOrder(numbers.Root);
+    }
+
 }
 
 // References https://www.youtube.com/watch?v=oSWTXtMglKE 
