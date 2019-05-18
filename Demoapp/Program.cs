7f7eb89b8aa7ec0123a87a72a6bc2be80e095910
minhduc2611 <minhduc261298@gmail.com>
2019-05-18 18:27:01 +0700

    binary search

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,94 +2,48 @@
 
 class demo
 {
-    // Binary tree 2
-
-
-    //node model
-    public class Node
+    // Binary search
+    public static int BinarySearch(int value, int[] Arr)
     {
-        public int Data;
-        public Node Left;
-        public Node Right;
-        public void DisplayNode()
-        {
-            Console.Write(Data + " ");
-        }
+        int LowBoundIndex = 0;
+        int HighBoundIndex = Arr.Length-1 ; // because lenght-1 = thelast index in array 
+        int MidlleIndex;
+
+        while( LowBoundIndex <= HighBoundIndex)// start moving pointers UNTIL they are equal or low > high
+        {// do this 
+            MidlleIndex = (LowBoundIndex+HighBoundIndex)/2; // find out the INT midle one ( not double )
+            if(Arr[MidlleIndex]==value)
+                return MidlleIndex;
+            else 
+                if(Arr[MidlleIndex] < value)
+                    LowBoundIndex = MidlleIndex++; // cut the front array => now is : new Low and High
+            else 
+                HighBoundIndex = MidlleIndex--; // cut the behind array == > now is : low and new High
+
+        } // the while loop stop and cant find the neeeded element.
+        return -1; // -1 represents as " cant find it "
+
     }
-    //binary tree model 
-    public class BinarySearchTree
-    {
-        public Node Root;
-        public BinarySearchTree()
-        {
-            this.Root = null;
-        }
-        public void Insert(int i)
-        {
-            Node NewNode = new Node() { Data = i };
-            if (this.Root == null)
-            {
-                this.Root = NewNode; //end
-            }
-            else
-            {
-                Node Current = Root; // pointer = current
-                Node Parent;
-                while (true)
-                {
-                    Parent = Current;
-                    if (i < Current.Data)
-                    {
-                        Current = Current.Left;
-                        if (Current == null)
-                        {
-                            Parent.Left = NewNode;
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        Current = Current.Right;
-                        if (Current == null)
-                        {
-                            Parent.Right = NewNode;
-                            break;
-                        }
-                    }
-                }
-            }
+    public static void FindElemenInArrayByBinarySearch(int value, int[] Arr){
 
-        }
-
-        public void InOrder(Node TheRoot)
-        {
-            if (!(TheRoot == null))
-            {
-                InOrder(TheRoot.Left);
-                TheRoot.DisplayNode();
-                InOrder(TheRoot.Right);
-            }
-        }
+        int result= BinarySearch(value,Arr);
+        if(result > -1 )
+            Console.WriteLine("the element " + value + " is at index " + result);
+        else
+            Console.WriteLine("can not find the element");
     }
 
     static void Main(string[] args)
-    {
+    {        
+        int[] arr = new int[]{1,23,34,45,56,67,78,89,123,234,345,456};
+        Console.WriteLine("the array : " );
 
-        //int[] arr = new int[] { 4, 2, 3, 5, 7, 2, 1, 7, 5 };
-      BinarySearchTree numbers = new BinarySearchTree();
-      numbers.Insert(23);
-      numbers.Insert(45);
-      numbers.Insert(26);
-      numbers.Insert(37);
-      numbers.Insert(3);
-      numbers.Insert(99);
-      numbers.Insert(21);
-      numbers.Insert(22);
+        for(int i = 0 ; i < arr.Length ; i++){
+            Console.WriteLine( " index "+i +" : "+ arr[i]);
+        }
+        FindElemenInArrayByBinarySearch(45,arr);
 
-      Console.WriteLine("InOrder Traversal:");
-      numbers.InOrder(numbers.Root);
     }
 
 }
 
-// References https://www.youtube.com/watch?v=oSWTXtMglKE 
