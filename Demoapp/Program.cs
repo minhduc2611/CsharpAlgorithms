4156496ac4538cbae12915b1fa6b6e7495517d8f
minhduc2611 <minhduc261298@gmail.com>
2019-05-19 15:11:40 +0700

    my quicksort

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -2,19 +2,9 @@
 
 class demo
 {
-    // Quick Sort
-
-    /*
-    *
-    * Quick sort follows recursive algorithm
-    *
-    * We find the pavot which devides the array into 2 halves 
-    *
-    *then we find the pavot for these halves ==> which is ultimately give us the sorted array 
-    */
     static void Main(string[] args)
-    {
-        int[] arr = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
+        {
+            int[] arr = { 67, 12, 95, 56, 85, 1, 100, 23, 60, 9 };
         int n = 10, i;
         Console.WriteLine("Quick Sort");
         Console.Write("Initial array is: ");
@@ -22,93 +12,58 @@ class demo
         {
             Console.Write(arr[i] + " ");
         }
-        quickSort(arr, 0, arr.Length - 1);
+        QuickSort(arr, 0, arr.Length - 1);
         Console.Write("\nSorted Array is: ");
         for (i = 0; i < n; i++)
         {
             Console.Write(arr[i] + " ");
         }
-    }
-    /*
-    *
-    *
-    *
-      funtion Partition
-      
-      Mục tiêu : các phàn tử bé hơn pivot value thì sang trái pivot, 
-                  nguọc lại các phần tử lớn hơn Pivotvalue thì sang phải 
-      Input : array, pointer BeginPointer, Pointer LastPointer.
-      Return : sau khi đã sort xong phần Mục_Tiêu, hàm sẽ trả lại vị trí, nơi mà pointer BeginPointer và LastPointer = nhau và = Pavot value 
-      
-      B1 : gán pivot vào phần tử đầu arr[BeginPointer]
-      B2 : lặp quá trình cho đến khi return được vị trí Pivot 
-            {
-               Step1: loop cho tới khi tìm thấy ptu lớn hơn pivot ( arr BeginPointer < pivot value ----> BeginPointer ++ ) chuyển sang bước 2
-               Step2: loop cho tới khi tìm thấy ptu nhỏ hơn pivot ( pivot value  < arr [LastPointer] ----> LastPointer -- ) . chuyển sang bước 3
-               step3: hoán đổi vị trí nếu Bước 2 và 3 đã loop xong (  phát hiện được ( có  ) )
-            }
-      *
-      *
-     */
-    static public int Partition(int[] arr, int BeginPointer, int LastPointer)
-    {
-        int PivotValue;
-        PivotValue = arr[BeginPointer];
-        while (true)
+
+        }
+        public static int Partition(int[] Arr, int LeftPointer,int RightPointer)
         {
-            while (arr[BeginPointer] < PivotValue)
-            {
-                BeginPointer++;
-            }
-            while (PivotValue < arr[LastPointer])
-            {
-                LastPointer--;
-            }
-            if (BeginPointer < LastPointer)
-            {
-                // swap 
-                int temp = arr[LastPointer];
-                arr[LastPointer] = arr[BeginPointer];
-                arr[BeginPointer] = temp;
-            }
-            else
-            {
-                return LastPointer;
+            int Pavot = Arr[LeftPointer];
+            int EndIndex;
+            while(true){
+                while(Arr[LeftPointer] < Pavot){
+                    LeftPointer ++;
+                }
+                while(Arr[RightPointer] > Pavot ){
+                    RightPointer--;
+                }
+                // loop end : find out the elements needed to be exchanged
+                if(LeftPointer<RightPointer){
+                //swap
+                    int temp = Arr[LeftPointer];
+                    Arr[LeftPointer] = Arr[RightPointer];
+                    Arr[RightPointer] = temp;
+                }
+                else{
+                    EndIndex = RightPointer;
+                    return EndIndex;
+                }
             }
         }
 
-    }
-
-    /*
-    *
-    *
-    *
-    funtion quickSort
-
-    Mục tiêu : xác định đúng vị trí phần tử đã được sort 
-    Input :array ,  2 con trỏ xác định đoạn arr cần sort là BeginPointer and LastPointer 
-    K có return, ARRay sẽ bị biến đổi trong lúc execute hàm. 
-    *
-    *
-   */
-    static public void quickSort(int[] arr, int BeginPointer, int LastPointer)
-    {
-        int Defined_PivotIndex; 
-        if (BeginPointer < LastPointer)
+        public static void QuickSort(int[] Arr, int LeftPointer,int RightPointer)
         {
-            Defined_PivotIndex = Partition(arr, BeginPointer, LastPointer);
-            // after defind  Pivotindex
-            // the First half of the array 
-            if (Defined_PivotIndex > 1)
-            {   
-                quickSort(arr, BeginPointer, Defined_PivotIndex - 1);
+            int Defined_Pavot_Index ;
+
+            Defined_Pavot_Index = Partition(Arr,LeftPointer,RightPointer);
+            // 2 halves 
+
+
+            // if pivot at NO# (0) ,  (1) -- > keep.  
+            if(Defined_Pavot_Index > 1){ // use if to restrict
+                QuickSort(Arr,LeftPointer, Defined_Pavot_Index-1);
             }
 
-            // the Second half of the array
-            if (Defined_PivotIndex + 1 < LastPointer)
-            { // make sure after the pivot is more than 1 elements 
-                quickSort(arr, Defined_PivotIndex + 1, LastPointer);
+            // if pivot at NO# (arr.lenght - 1) ,  (arr.lenght - 2) -- > keep.  
+            if(Defined_Pavot_Index + 1< RightPointer )
+            {
+                QuickSort(Arr,Defined_Pavot_Index+1, RightPointer);
             }
+
         }
-    }
+
 }
