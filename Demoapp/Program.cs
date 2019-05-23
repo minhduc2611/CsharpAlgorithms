067a7f09c6a779b506c427eadf133b02e95e1715
minhduc2611 <minhduc261298@gmail.com>
2019-05-23 11:02:48 +0700

    selection sort

--- Demoapp/Program.cs
+++ Demoapp/Program.cs
@@ -3,27 +3,49 @@
 class demo
 {
     // linked list
-    public static void bubbleSort(int[] Arr)
-    {
-        Boolean isSorted = false;
-        int LastUnsortIndex = Arr.Length - 1;
-        while (!isSorted)
-        {
-            isSorted = true;
-            for (int i = 0; i < LastUnsortIndex; i++)
-            {
-                if (Arr[i] > Arr[i + 1])
-                {
-                    //swap(Arr, Arr[i], Arr[i + 1]);
-                 int temp = Arr[i];
-                Arr[i] = Arr[i+1];
-                Arr[i+1] = temp;
+    public static void SelectionSort(int[] Arr){
+        // int i=0;
+        // while(i< Arr.Length-1){
+        //     int PointerFirstIndex = i;
+        //     int PointerSecondIndex = i+1;
+        //     int PointerMinValue=Arr[i+1];
+        //     int MinIndex = i+1;
+        //     for(int PointerMinIndex = PointerSecondIndex+1;PointerMinIndex<Arr.Length-1;PointerMinIndex++){
+        //         if(Arr[PointerMinIndex] < PointerMinValue)
+        //             MinIndex = PointerMinIndex;
+            
+        //     }
+        //         if(PointerMinValue < PointerFirstIndex){
+        //             swap(Arr,MinIndex,PointerFirstIndex);
+        //         }
+        //         i++;
+        // }
+
+        // int SortedIndex = 0;
+        // while(SortedIndex < Arr.Length-1){
+        //     int Min = Arr[0];
+        //     int MinIndex = 0;
+        //     for(int i =0; i< Arr.Length-1;i++){
+        //         if(Min > Arr[i]){
+        //             Min = Arr[i];
+        //             MinIndex = i;
+        //         }
+        //     }
+        //     swap(Arr,SortedIndex,MinIndex);
+        //     SortedIndex ++;
+        // }
+
+        int Min;
+        for(int i = 0; i < Arr.Length; i++){
+            Min = i;
+            for(int j = i+1; j < Arr.Length; j ++){
+                if(Arr[Min]>Arr[j]){
+                    swap(Arr,Min,j);
                 }
-                isSorted = false;
             }
-            LastUnsortIndex--;
         }
     }
+
     public static void swap(int[] Arr, int a, int b)
     {
         int temp = Arr[a];
@@ -33,13 +55,13 @@ class demo
 
     static void Main(string[] args)
     {
-        int[] Arr = new int[] { 17, 3, 9, 45, 12, 4, 5, 23 };
-
-        bubbleSort(Arr);
-
-        for(int a = 0; a< Arr.Length;a++){
-        Console.Write(Arr[a]+" ");
+        int[] Arr = new int[]{7,3,9,8,4,1,5,6};
+        SelectionSort(Arr);
+        for(int i =0;i < Arr.Length; i++){
+            Console.Write(Arr[i]+" ");
         }
+        // Console.Write(SelectionSort(Arr));
+
     }
 
 }
